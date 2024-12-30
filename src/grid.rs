67fd5f159d4bf5e987551c1d00bd8f2a26f1b127
Jack Weber <jackweber142@gmail.com>
2024-12-30 05:24:09 -0500

    fixed simulation grid interactions, just need to update g2p particle boundaries

---
+++ src/grid.rs
@@ -0,0 +1,289 @@
+use std::ops::{Deref, DerefMut};
+use std::sync::Mutex;
+use std::collections::HashMap;
+use crate::simulation::Node;
+
+use bevy::prelude::*;
+
+///
+/// A chunk is an N x N x N block of eulerian nodes that make up a larger fluid simulation space
+/// N = Chunk_Size
+///
+/// A node is uniquely identified in a chunk by its local coordinates (0..N, 0..N, 0..N)
+/// These coordinates get flattened down into a 1d usize index and accessed in the array
+///
+/// A chunk is uniquely identified in a Grid by it's chunk coordinates (x, y, z) 
+/// These coordinate are accessed through a hashmap
+///
+/// The chunk is responsible for Nodes from (x*N, y*N, z*N) to (((x+1)*N)-1, ((y+1)*N)-1, ((z+1)*N)-1)
+/// EXAMPLE for N = 16:
+/// Chunk (0, 0, 0) is responsible for (0, 0, 0) -> (15, 15, 15)
+/// Chunk (-1, -1, -1) is responsible for (-16, -16, -16) -> (-1, -1, -1)
+///
+pub struct Chunk { 
+    pub g: [Node; Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE],
+    pub edge_mask: u8,
+}
+
+impl Chunk {
+    // NOTE: It's assumed Chunk_size < i32_max && Chunk_Size > Edge_Buffer_Size
+    // Consider changing to u32 or u16
+    pub const CHUNK_SIZE: usize = 4; // Number of nodes in a chunk
+    pub const EDGE_BUFFER_SIZE: usize = 2; // How big of boundary will edge conditions be applied to
+    //pub const DX: f32 = 1.; // The difference between each node orthogonally
+
+    /// 
+    /// Input:
+    /// World Pos of Node
+    ///
+    /// Output:
+    /// Chunk coords of chunk responsible for that Node
+    ///        
+    /// Test Written and Passed
+    pub fn node_world_pos_to_chunk_pos(nwp: IVec3) -> IVec3 {
+        let ichunk = Chunk::CHUNK_SIZE as i32; 
+        // Adjusts for off by 1 for negatives because np 0,0,0 is in cp 0,0,0
+        IVec3::new(
+            if nwp.x < 0 { (nwp.x + 1) / ichunk - 1 } else { nwp.x / ichunk },
+            if nwp.y < 0 { (nwp.y + 1) / ichunk - 1 } else { nwp.y / ichunk },
+            if nwp.z < 0 { (nwp.z + 1) / ichunk - 1 } else { nwp.z / ichunk }
+        )
+    }
+
+    ///
+    /// Input:
+    /// World Pos of Node
+    ///
+    /// Output:
+    /// Index of node in its chunk
+    ///
+    /// Test Written and Passed
+    pub fn node_world_pos_to_index(nwp: IVec3) -> usize {
+        let ichunk = Chunk::CHUNK_SIZE as i32;
+        let nlp = nwp.rem_euclid(IVec3::splat(ichunk)); // Node World Position => Node Local Position 
+        // TODO: See if Morton Encoding is more efficient
+        nlp.z as usize * Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE + nlp.y as usize * Chunk::CHUNK_SIZE + nlp.x as usize
+    }
+
+    ///
+    /// Input:
+    /// The local position of a Node
+    ///
+    /// Output:
+    /// The edge mask of the current edges of the chunk a node is on
+    ///
+    /// Test Written and Passed
+    pub fn node_local_pos_to_edge_mask(nlp: UVec3) -> u8 {
+        let mut edge_mask: u8 = 0;
+
+        if (nlp.y as usize) < Chunk::EDGE_BUFFER_SIZE { edge_mask |= 1; } // 1
+        else if (nlp.y as usize) > Chunk::CHUNK_SIZE - (Chunk::EDGE_BUFFER_SIZE + 1) { edge_mask |= 1 << 5; } // 6
+        if (nlp.z as usize) < Chunk::EDGE_BUFFER_SIZE { edge_mask |= 1 << 1; } // 2
+        else if (nlp.z as usize) > Chunk::CHUNK_SIZE - (Chunk::EDGE_BUFFER_SIZE + 1) { edge_mask |= 1 << 4; } // 5
+        if (nlp.x as usize) < Chunk::EDGE_BUFFER_SIZE { edge_mask |= 1 << 2; } // 3
+        else if (nlp.x as usize) > Chunk::CHUNK_SIZE - (Chunk::EDGE_BUFFER_SIZE + 1) { edge_mask |= 1 << 3; } // 4
+
+        edge_mask
+    }
+
+    ///
+    /// Input:
+    /// The index of a node
+    ///
+    /// Output:
+    /// The local position of the node inside its chunk
+    ///
+    /// Test Written and Passed
+    pub fn index_to_node_local_pos(i: usize) -> UVec3 {
+        let c2 = Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE;
+        let z = i / c2;
+        let y = (i % c2) / Chunk::CHUNK_SIZE;
+        let x = i % Chunk::CHUNK_SIZE;
+        
+        UVec3::new(x as u32, y as u32, z as u32)
+    }
+
+    pub fn new(edge_mask: u8) -> Self {
+        Chunk{
+            g: [Node::default(); Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE],
+            edge_mask
+        }
+    }
+}
+
+impl Deref for Chunk {
+    type Target = [Node; Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE * Chunk::CHUNK_SIZE];
+    fn deref(&self) -> &Self::Target { &self.g } 
+}
+
+impl DerefMut for Chunk { fn deref_mut(&mut self) -> &mut Self::Target { &mut self.g }  }
+
+// Eulerian Grid Container 
+#[derive(Resource)]
+pub struct Grid (pub HashMap<IVec3, Mutex<Chunk>>);
+
+impl Grid {
+    /// Inserts a new chunk at pos with its correct edge_mask
+    /// Test Written and Passed
+    pub fn new_chunk(&mut self, pos: &IVec3) {
+        // If the chunk is already in the hashmap
+        if let Some(_) = self.get(pos) {
+            panic!("used new_chunk for already initilized chunk");
+        }
+        //
+        //  Edge mask formatted like a dice
+        //      +--------+
+        //     /        /|
+        //    /    6   / |
+        //   +--------+  |
+        //   |        | 4|
+        //   |        |  +
+        //   |    5   | /
+        //   |        |/
+        //   +--------+
+        // 1 (0, -1, 0)  
+        // 2 (0, 0, -1)
+        // 3 (-1, 0, 0)
+        // 4 (+1, 0, 0)
+        // 5 (0, 0, +1)
+        // 6 (0, +1, 0)
+        //
+        let mut edge_mask = 0;
+
+        // If there is a chunk on the border update its edge mask
+        // Otherwise update your own edge mask
+
+        if let Some(c) = self.get(&(pos + IVec3::new(0, -1, 0))) {
+            let mut chunk = c.lock().expect("Error locking chunk in new_chunk");
+            chunk.edge_mask &= 0b011111;
+        } else {edge_mask |= 1;}
+        if let Some(c) = self.get(&(pos + IVec3::new(0, 0, -1))) {
+            let mut chunk = c.lock().expect("Error locking chunk in new_chunk");
+            chunk.edge_mask &= 0b101111;
+        } else {edge_mask |= 1 << 1}
+        if let Some(c) = self.get(&(pos + IVec3::new(-1, 0, 0))) {
+            let mut chunk = c.lock().expect("Error locking chunk in new_chunk");
+            chunk.edge_mask &= 0b110111;
+        } else {edge_mask |= 1 << 2}
+        if let Some(c) = self.get(&(pos + IVec3::new(1, 0, 0))) {
+            let mut chunk = c.lock().expect("Error locking chunk in new_chunk");
+            chunk.edge_mask &= 0b111011;
+        } else {edge_mask |= 1 << 3}
+        if let Some(c) = self.get(&(pos + IVec3::new(0, 0, 1))) {
+            let mut chunk = c.lock().expect("Error locking chunk in new_chunk");
+            chunk.edge_mask &= 0b111101;
+        } else {edge_mask |= 1 << 4}
+        if let Some(c) = self.get(&(pos + IVec3::new(0, 1, 0))) {
+            let mut chunk = c.lock().expect("Error locking chunk in new_chunk");
+            chunk.edge_mask &= 0b111110;
+        } else {edge_mask |= 1 << 5}
+        
+        let chunk = Chunk::new(edge_mask);
+        self.insert(*pos, Mutex::new(chunk));
+    }
+}
+
+impl Deref for Grid {
+    type Target = HashMap<IVec3, Mutex<Chunk>>;
+    fn deref(&self) -> &Self::Target { &self.0 } 
+}
+impl DerefMut for Grid { fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 } }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn node_world_pos_to_chunk_pos_test() {
+        let n = Chunk::CHUNK_SIZE as i32;
+
+        // 0, 0, 0 should be responsible for 0, 0, 0 up to (n - 1, n - 1, n - 1)
+        assert!(Chunk::node_world_pos_to_chunk_pos(IVec3::ZERO) == IVec3::ZERO);
+        assert!(Chunk::node_world_pos_to_chunk_pos(IVec3::splat(n - 1)) == IVec3::ZERO);
+
+        // -1, -1, -1 should be responsible for (-n, -n, -n) up to (-1, -1, -1)
+        assert!(Chunk::node_world_pos_to_chunk_pos(IVec3::splat(-1)) == IVec3::splat(-1));
+        assert!(Chunk::node_world_pos_to_chunk_pos(IVec3::splat(-n)) == IVec3::splat(-1));
+
+        // 1, 1, 1 should be responsible for (n, n, n) up to (2n - 1, 2n - 1, 2n -1)
+        assert!(Chunk::node_world_pos_to_chunk_pos(IVec3::splat(n)) == IVec3::splat(1));
+        assert!(Chunk::node_world_pos_to_chunk_pos(IVec3::splat((2 * n) - 1)) == IVec3::splat(1));
+    }
+
+    #[test]
+    pub fn node_world_pos_to_index_test_linear() {
+        let n = Chunk::CHUNK_SIZE as i32;
+
+        // (0, 0, 0) should be 0
+        assert!(Chunk::node_world_pos_to_index(IVec3::ZERO) == 0);
+        // (n, n, n) should be 0
+        assert!(Chunk::node_world_pos_to_index(IVec3::splat(n)) == 0);
+        // (0, 0, 1) should be 1
+        assert!(Chunk::node_world_pos_to_index(IVec3::from((1, 0, 0))) == 1);
+        // (-n, -n, -n) should be 0
+        assert!(Chunk::node_world_pos_to_index(IVec3::splat(-n)) == 0);
+        // (-1, -1, -1) should be n * n * n
+        assert!(Chunk::node_world_pos_to_index(IVec3::splat(-1)) == (n * n * n - 1) as usize);
+    }
+
+    #[test]
+    fn node_local_pos_to_edge_mask_test() {
+        let n = Chunk::CHUNK_SIZE as u32;
+
+        // (0, 0, 0) should be 111
+        assert!(Chunk::node_local_pos_to_edge_mask(UVec3::ZERO) == 0b111);
+        // (n -1, n - 1, n - 1) should be 111000
+        assert!(Chunk::node_local_pos_to_edge_mask(UVec3::splat(n - 1)) == 0b111000);
+        // (n - 1, 0, 0) should be 1011
+        assert!(Chunk::node_local_pos_to_edge_mask(UVec3::new(n - 1, 0, 0)) == 0b1011);
+    }
+
+    #[test]
+    fn index_to_node_local_pos() {
+        let n = Chunk::CHUNK_SIZE as u32;
+
+        // 0 should be (0, 0, 0)
+        assert!(Chunk::index_to_node_local_pos(0) == UVec3::ZERO);
+        // n * n * n - 1 should be n - 1, n - 1, n - 1
+        assert!(Chunk::index_to_node_local_pos((n * n * n) as usize - 1) == UVec3::splat(n - 1));
+        // n - 1 should be (n - 1, 0, 0)
+        assert!(Chunk::index_to_node_local_pos((n) as usize - 1) == UVec3::new(n - 1, 0, 0));
+    }
+
+    #[test]
+    fn single_grid_chunk() {
+        let mut grid = Grid(HashMap::new());
+        grid.new_chunk(&IVec3::ZERO);
+
+        let chunk = grid.get(&IVec3::ZERO).unwrap().lock().unwrap();
+        assert!(chunk.edge_mask == 0b111111)
+    }
+
+    #[test]
+    fn orthogonal_grid_chunk() {
+        let mut grid = Grid(HashMap::new());
+        grid.new_chunk(&IVec3::ZERO);
+        grid.new_chunk(&IVec3::new(0, 0, 1));
+        grid.new_chunk(&IVec3::new(0, 1, 0));
+        grid.new_chunk(&IVec3::new(1, 0, 0));
+        grid.new_chunk(&IVec3::new(0, 0, -1));
+        grid.new_chunk(&IVec3::new(0, -1, 0));
+        grid.new_chunk(&IVec3::new(-1, 0, 0));
+
+        let chunk0 = grid.get(&IVec3::ZERO).unwrap().lock().unwrap();
+        let chunk1 = grid.get(&IVec3::new(0, 0, 1)).unwrap().lock().unwrap();
+        let chunk2 = grid.get(&IVec3::new(0, 1, 0)).unwrap().lock().unwrap();
+        let chunk3 = grid.get(&IVec3::new(1, 0, 0)).unwrap().lock().unwrap();
+        let chunk4 = grid.get(&IVec3::new(0, 0, -1)).unwrap().lock().unwrap();
+        let chunk5 = grid.get(&IVec3::new(0, -1, 0)).unwrap().lock().unwrap();
+        let chunk6 = grid.get(&IVec3::new(-1, 0, 0)).unwrap().lock().unwrap();
+        println!("{:b}", chunk0.edge_mask);
+        assert!(chunk0.edge_mask == 0b0);
+        assert!(chunk1.edge_mask == 0b111101);
+        assert!(chunk2.edge_mask == 0b111110);
+        assert!(chunk3.edge_mask == 0b111011);
+        assert!(chunk4.edge_mask == 0b101111);
+        assert!(chunk5.edge_mask == 0b11111);
+        assert!(chunk6.edge_mask == 0b110111);
+    }
+}
---
+++ src/simulation.rs
@@ -0,0 +1,355 @@
+#![allow(non_upper_case_globals)]
+
+use rayon::prelude::*;
+use bevy::{prelude::*, gizmos::gizmos, color::palettes::{tailwind::{RED_500, BLUE_500}, css::GHOST_WHITE}};
+
+use crate::grid::*;
+
+const dt: f32 = 0.4;
+const gravity: f32 = -0.3;
+const rest_density: f32 = 4.0;
+const dynamic_viscosity: f32 = 0.1;
+const eos_stiffness: f32 = 10.0;
+const eos_power: f32 = 4.;
+
+
+/// Langrangian Particle
+#[derive(Component)]
+pub struct Particle {
+    x: Vec3,    // Position
+    v: Vec3,    // Velocity
+    c: Mat3,    // Affine Momentum Matrix
+    m: f32,     // Mass
+    p: u16    // material prime index
+}
+
+/// Eulerian Grid Node
+#[derive(Copy, Clone)]
+pub struct Node {
+    v: Vec3,    // Velocity
+    m: f32,     // Mass
+    p: u32,     // Material Number
+}
+
+impl Default for Node {
+    fn default() -> Self {
+        Node { v: Vec3::ZERO, m: 0., p: 0}
+    }
+}
+
+impl Node {
+    fn zero(&mut self) {
+        self.p = 0;
+        self.m = 0.0;
+        self.v = Vec3::ZERO;
+    }
+}
+
+
+/// Sets all nodes in the grid to 0
+pub fn clear_grid ( mut grid: ResMut<Grid> ) {
+    grid.par_iter_mut().for_each(|(_, c)| {
+        let mut chunk = c.lock().expect("Error locking Mutex for clear_grid");
+        chunk.iter_mut().for_each(|node| {
+            node.zero();
+        });
+    });
+}
+
+pub fn p2g1 (
+    grid: ResMut<Grid>,
+    query: Query<&Particle>,
+    ) {
+    //.par_iter_mut()
+    query.par_iter().for_each(|p| {
+        let n = p.x.trunc(); // Truncates decimal part of float, leaving integer part
+        let dx = p.x.fract() - 0.5; // How far away the particle is away from the node
+
+        let w = [
+            0.5 * (0.5 - dx).powf(2.),
+            0.75 - (dx).powf(2.),
+            0.5 * (0.5 + dx).powf(2.),
+        ];
+
+        // Calculate contributions of langrangian particle to euclidean grid
+        for i in 0..3 { 
+            for j in 0..3 {
+                for k in 0..3 {
+                    let weight = w[i].x * w[j].y * w[k].z; // Quadratic B-Spline  
+                    let curr_x = Vec3::from([
+                                            n.x + (i as f32) - 1.,
+                                            n.y + (j as f32) - 1.,
+                                            n.z + (k as f32) - 1.,
+                    ]);
+                    let icurr_x = curr_x.as_ivec3();
+                    let curr_dx = (curr_x - p.x) + 0.5; // p distance to current n
+
+
+                    // Pre-calculate values we will send to mutex
+                    let q = p.c * curr_dx;
+                    let mass_contrib = weight * p.m;
+                    let v_contrib = mass_contrib * (p.v + Vec3::from(q));
+
+                    // Get Mutex
+                    let chunk_index = Chunk::node_world_pos_to_chunk_pos(icurr_x); 
+                    let n_index = Chunk::node_world_pos_to_index(icurr_x);
+                    let mut chunk = grid.get(&chunk_index).expect("Particle somehow out of bounds of loaded chunks").lock().expect("Error locking mutex for p2g");
+
+                    // Do work inside mutex
+                    chunk[n_index].m += mass_contrib;
+                    chunk[n_index].v += v_contrib;
+
+                    std::mem::drop(chunk); // Unlock Mutex
+                }
+            }
+        }
+    });
+}
+
+pub fn p2g2 (
+    grid: ResMut<Grid>,
+    query: Query<&Particle>,
+    ) {
+    query.par_iter().for_each(|p| {
+        let n = p.x.trunc(); // Truncates decimal part of float, leaving integer part
+        let dx = p.x.fract() - 0.5; // How far away the particle is away from the node
+
+        let w = [
+            0.5 * (0.5 - dx).powf(2.),
+            0.75 - (dx).powf(2.),
+            0.5 * (0.5 + dx).powf(2.),
+        ];
+
+        let mut density: f32 = 0.;
+        // Calculate contributions of langrangian particle to euclidean grid
+        for i in 0..3 { 
+            for j in 0..3 {
+                for k in 0..3 {
+                    let weight = w[i].x * w[j].y * w[k].z; // Quadratic B-Spline  
+                    let curr_x = Vec3::from([
+                                            n.x + (i as f32) - 1.,
+                                            n.y + (j as f32) - 1.,
+                                            n.z + (k as f32) - 1.,
+                    ]);
+                    let icurr_x = curr_x.as_ivec3();
+
+                    // Get Mutex
+                    let chunk_index = Chunk::node_world_pos_to_chunk_pos(icurr_x); 
+                    let n_index = Chunk::node_world_pos_to_index(icurr_x);
+                    let mut chunk = grid.get(&chunk_index).expect("Particle somehow out of bounds of loaded chunks").lock().expect("Error locking mutex for p2g");
+
+                    // Do work inside mutex
+                    density += chunk[n_index].m * weight;
+
+                    std::mem::drop(chunk); // Unlock Mutex
+                }
+            }
+        }
+
+        let volume = p.m / density;
+        let pressure = (-0.1_f32).max(eos_stiffness * (density / rest_density).powf(eos_power) - 1.);
+        // Identity Matrix
+        let mut stress = Mat3::from_cols_array(&[
+                                               -pressure, 0., 0., 
+                                               0., -pressure, 0.,
+                                               0., 0., -pressure,
+        ]);
+        let mut strain = p.c;
+
+        let trace = strain.x_axis.x + strain.y_axis.y + strain.z_axis.z;
+        strain.x_axis.x = trace;
+        strain.y_axis.y = trace;
+        strain.z_axis.z = trace;
+
+        let viscosity_term = dynamic_viscosity * strain;
+        stress += viscosity_term;
+
+        let eq_16_term_0 = -volume * 4. * stress * dt;
+
+        for i in 0..3 { 
+            for j in 0..3 {
+                for k in 0..3 {
+                    let weight = w[i].x * w[j].y * w[k].z; // Quadratic B-Spline  
+                    let curr_x = Vec3::from([
+                                            n.x + (i as f32) - 1.,
+                                            n.y + (j as f32) - 1.,
+                                            n.z + (k as f32) - 1.,
+                    ]);
+                    let icurr_x = curr_x.as_ivec3();
+                    let curr_dx = (curr_x - p.x) + 0.5; // p distance to current n
+                                                        
+                    let momentum = Vec3::from(eq_16_term_0 * weight * curr_dx);
+
+                    // Get Mutex
+                    let chunk_index = Chunk::node_world_pos_to_chunk_pos(icurr_x); 
+                    let n_index = Chunk::node_world_pos_to_index(icurr_x);
+                    let mut chunk = grid.get(&chunk_index).expect("Particle somehow out of bounds of loaded chunks").lock().expect("Error locking mutex for p2g");
+
+
+                    // Do work inside mutex
+                    density += chunk[n_index].m * weight;
+                    chunk[n_index].v += momentum;
+
+                    std::mem::drop(chunk); // Unlock Mutex
+                }
+            }
+        }
+    });
+}
+
+//
+//  Edge mask formatted like a dice
+//      +--------+
+//     /        /|
+//    /    6   / |
+//   +--------+  |
+//   |        | 4|
+//   |        |  +
+//   |    5   | /
+//   |        |/
+//   +--------+
+// 1 (0, -1, 0)  
+// 2 (0, 0, -1)
+// 3 (-1, 0, 0)
+// 4 (+1, 0, 0)
+// 5 (0, 0, +1)
+// 6 (0, +1, 0)
+//
+pub fn update_grid ( grid: Res<Grid>) {
+
+    grid.par_iter().for_each(|(_, c)| {
+        let mut chunk = c.lock().expect("Error locking Mutex for update_grid");
+        let chunk_edge_mask = chunk.edge_mask;
+
+        chunk.iter_mut().enumerate().for_each(|(i, node)| {
+            // Boundary condition
+            let nlp = Chunk::index_to_node_local_pos(i);
+
+
+            if node.m > 0. {
+                node.v /= node.m;
+                node.v.y += dt * gravity;
+            }
+
+            if chunk_edge_mask != 0 {
+                let node_edge_mask = Chunk::node_local_pos_to_edge_mask(nlp);
+                if node_edge_mask & chunk_edge_mask != 0 {node.v = Vec3::ZERO};
+            }
+        });
+    });
+}
+
+pub fn g2p (
+    grid: Res<Grid>,
+    mut particles: Query<&mut Particle>
+) {
+   particles.par_iter_mut().for_each(|mut p| {
+        p.v = Vec3::ZERO;
+
+        let n = p.x.trunc(); // Truncates decimal part of float, leaving integer part
+        let dx: Vec3 = p.x.fract() - 0.5; // How far away the particle is away from the node
+        
+        let w: [Vec3; 3]= [
+            0.5 * (0.5 - dx).powf(2.),
+            0.75 - (dx).powf(2.),
+            0.5 * (0.5 + dx).powf(2.),
+        ];
+
+        let mut b = Mat3::ZERO;
+        let mut edge_mask = 0; 
+        for i in 0..3 {
+            for j in 0..3 {
+                for k in 0..3 {
+                    let weight = w[i].x * w[j].y * w[k].z; // Quadratic B-Spline  
+                    let curr_x = Vec3::from([
+                        n.x + (i as f32) - 1.,
+                        n.y + (j as f32) - 1.,
+                        n.z + (k as f32) - 1.,
+                    ]);
+                    let icurr_x = curr_x.as_ivec3();
+                    let curr_dx = (curr_x - p.x) + 0.5; // p distance to current n
+                    // Get Mutex
+                    let chunk_index = Chunk::node_world_pos_to_chunk_pos(icurr_x); 
+                    let n_index = Chunk::node_world_pos_to_index(icurr_x);
+                    let chunk = grid.get(&chunk_index).expect("Particle somehow out of bounds of loaded chunks").lock().expect("Error locking mutex for p2g");
+
+                    // Do work inside mutex
+                    let mut w_v = chunk[n_index].v;
+                    
+                    // Get the edge_mask for free while locking the first time
+                    if i == 0 && j == 0 && k == 0 {
+                        edge_mask = chunk.edge_mask;
+                    }
+
+                    std::mem::drop(chunk); // Unlock Mutex
+
+                    w_v *= weight;
+                    let term = Mat3::from_cols(w_v * curr_dx.x, w_v * curr_dx.y, w_v * curr_dx.z);
+                    b += term;
+                    p.v += w_v;
+                }
+            }
+        }
+        p.c = b.mul_scalar(4.);
+
+        let end = (16 - (Chunk::EDGE_BUFFER_SIZE + 2)) as f32;
+
+        let local_pos = Chunk::index_to_node_local_pos(Chunk::node_world_pos_to_index(n.as_ivec3()));
+
+      // if p.x.y < 3. {p.v.y += Chunk::EDGE_BUFFER_SIZE as f32 - local_pos.y as f32} // 1
+      // if p.x.z < 3. {p.v.z += Chunk::EDGE_BUFFER_SIZE as f32 - local_pos.z as f32} // 2
+      // if p.x.x < 3. {p.v.x += Chunk::EDGE_BUFFER_SIZE as f32 - local_pos.x as f32} // 3
+      // if p.x.x > end {p.v.x += end - local_pos.x as f32} //4 
+      // if p.x.z > end {p.v.z += end - local_pos.z as f32} // 5
+      // if p.x.y > end {p.v.y += end - local_pos.y as f32} // 6
+
+        let v = p.v;
+        p.x += v * dt; 
+        // println!("{}", p.x);
+    })
+}
+
+pub fn initialize(
+    mut commands: Commands,
+    mut grid: ResMut<Grid>,
+) {
+    for i in 0..4 {
+        for j in 0..4 { 
+            for k in 0..4 { 
+                grid.new_chunk(&IVec3::new(i, j, k));
+            }
+        }
+    }
+
+    for x in 3..13 {
+        for y in 3..13{
+            for z in 3..13 {
+                commands.spawn(Particle {
+                    x: Vec3::new(x as f32, y as f32, z as f32),
+                    v: Vec3::ZERO,
+                    c: Mat3::ZERO,
+                    p: 0,
+                    m: 1.
+                });
+            }
+        }
+    }
+}
+
+pub fn draw(
+    particles: Query<&Particle>,
+    mut gizmos: Gizmos,
+) {
+    for i in 0..5 {
+        for j in 0..5 {
+            for k in 0..5 {
+                gizmos.sphere(Vec3::from((i as f32, j as f32, k as f32)) * 4., 0.05, GHOST_WHITE);
+    }}}
+    gizmos.cuboid( 
+        Transform::from_translation(Vec3::from((8., 8., 8.))).with_scale(Vec3::splat(16.)),
+        GHOST_WHITE
+        );
+    particles.iter().for_each(|p| {
+        gizmos.sphere(p.x, 0.05, RED_500);
+    });
+}
